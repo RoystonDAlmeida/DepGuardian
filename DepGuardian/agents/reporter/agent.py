57bff87e0c252118ad684c17581269f2c185b42c
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-11-07 22:56:17 +0530

    Removed tool call

--- DepGuardian/agents/reporter/agent.py
+++ DepGuardian/agents/reporter/agent.py
@@ -1,6 +1,5 @@
 from google.adk.agents import Agent
 from DepGuardian.agents.reporter.prompt import REPORTER_INSTRUCTION 
-from DepGuardian.agents.reporter.tools.save_generated_report import save_generated_report_json
 
 # The model to use for reporting
 GEMINI_MODEL = "gemini-2.5-flash"
@@ -9,6 +8,5 @@ reporter_agent = Agent(
     name="ReporterAgent",
     model=GEMINI_MODEL,
     instruction=REPORTER_INSTRUCTION,
-    tools=[save_generated_report_json],
-    output_key="final_report",
+    output_key="final_report"
 )
