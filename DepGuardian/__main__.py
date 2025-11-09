5e532e9697ffdb268805d024fbf8465b33199feb
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-11-09 15:08:07 +0530

    Added port info

--- DepGuardian/__main__.py
+++ DepGuardian/__main__.py
@@ -5,5 +5,6 @@ from DepGuardian.task_manager import run
 app = create_app()
 
 if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, port=8000)
+    import uvicorn, os
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
