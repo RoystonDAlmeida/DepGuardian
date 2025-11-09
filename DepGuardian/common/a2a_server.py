02720f213be09048d26a58d7d3d779d69a9ee596
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-11-09 15:08:57 +0530

    Added /artifacts endpoint

--- DepGuardian/common/a2a_server.py
+++ DepGuardian/common/a2a_server.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, UploadFile, File
 from contextlib import asynccontextmanager
 
-from DepGuardian.agent import initialize_session, execute
+from DepGuardian.agent import initialize_session, execute, artifact_service, APP_NAME, USER_ID, SESSION_ID
 
 
 @asynccontextmanager
@@ -13,12 +13,14 @@ async def lifespan(app: FastAPI):
 def create_app():
     app = FastAPI(lifespan=lifespan)
 
-    @app.on_event("startup")
-    async def startup_event():
-        await initialize_session()       # ✅ FIXED
-
     @app.post("/run")
     async def run(file: UploadFile = File(...)):
         return await execute(file)       # ✅ file input
+
+    @app.get("/artifacts")
+    async def list_artifacts():
+        """Return list of stored artifacts for the current session."""
+        artifacts = await artifact_service.list_artifact_keys(app_name=APP_NAME, user_id = USER_ID, session_id=SESSION_ID)
+        return {"artifacts": artifacts}
 
     return app
