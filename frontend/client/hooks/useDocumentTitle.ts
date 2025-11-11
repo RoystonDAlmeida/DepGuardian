86c7e27e0276216cba31e0048e56ce8f12deb4e6
RoystonDAlmeida <roystonjan1@hotmail.com>
2025-11-11 11:26:51 +0530

    Implemented useDocumentTitle hook

---
+++ frontend/client/hooks/useDocumentTitle.ts
@@ -0,0 +1,18 @@
+// hooks/useDocumentTitle.ts
+import { useEffect, useRef } from "react";
+
+export function useDocumentTitle(title: string, retainOnUnmount = false) {
+  const defaultTitle = useRef(document.title);
+
+  useEffect(() => {
+    document.title = title;
+  }, [title]);
+
+  useEffect(() => {
+    return () => {
+      if (!retainOnUnmount) {
+        document.title = defaultTitle.current;
+      }
+    };
+  }, [retainOnUnmount]);
+}
